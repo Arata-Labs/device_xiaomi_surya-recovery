8b204be0c71864a9b2ead2e29a0cd3426bc8620b
HinohArata <mmgcntk@gmail.com>
2025-11-01 17:24:43 +0700

    customfeatures: Move from Android.mk
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- features/dfe/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-    LOCAL_MODULE := dfe_prebuilt
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_SRC_FILES := force_disable_encryption.sh
-    LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/customfeatures/dfe
-    LOCAL_MODULE_CLASS := EXECUTABLES
-    LOCAL_MODULE_MODE := 0755
-include $(BUILD_PREBUILT)
